6b2757f4d697ea6ff93c0a2933a79dffb301d1fc
Ara Ventura <ara.venturafitch@gmail.com>
2024-09-10 20:38:38 -0700

    create dental offices table

---
+++ migrations/01_create_dental_offices_table.js
@@ -0,0 +1,27 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+//Number next to the string declares the maximum characters per string
+
+export function up(knex) {
+  return knex.schema.createTable("DentalOffices", (table) => {
+    table.increments("id").primary();
+    table.string("name", 255).notNullable();
+    table.string("address", 255).notNullable();
+    table.string("practicingDoctor", 255).notNullable();
+    table.string("contactName", 255).notNullable();
+    table.string("contactPosition", 100).notNullable();
+    table.string("phone", 20).notNullable();
+    table.string("email", 100).notNullable();
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+
+export function down(knex) {
+  return knex.schema.dropTable("DentalOffices");
+}
